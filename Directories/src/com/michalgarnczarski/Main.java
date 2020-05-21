f3780e7a4f6fdc810ae956ca88bbf7a1a40b5074
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-21 17:45:39 +0200

    Video 262 finished

--- Directories/src/com/michalgarnczarski/Main.java
+++ Directories/src/com/michalgarnczarski/Main.java
@@ -58,12 +58,43 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("----Copy Dir2 to Dir4/Dir2Copy");
+        System.out.println("----Copy Dir2 to Dir4/Dir2Copy----");
         Path copyPath = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir4" + File.separator + "Dir2Copy");
         try {
             Files.walkFileTree(dir2Path, new CopyFiles(dir2Path, copyPath));
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+
+        File file = new File("C:\\Examples\\file.txt");
+        Path convertedPath = file.toPath();
+        System.out.println("convertedPath = " + convertedPath);
+
+        File parent = new File("C:\\Examples");
+        File resolvedFile = new File(parent, "dir\\file.txt");
+        System.out.println(resolvedFile.toPath());
+
+        resolvedFile = new File("C:\\Examples", "dir\\file.txt");
+        System.out.println(resolvedFile.toPath());
+
+        Path parentPath = Paths.get("C:\\Examples");
+        Path childRelativePath = Paths.get("dir\\file.txt");
+        System.out.println(parentPath.resolve(childRelativePath));
+
+        File workingDirectory = new File("").getAbsoluteFile();
+        System.out.println("Working directory = " + workingDirectory.getAbsolutePath());
+
+        System.out.println("----print Dir2 contents using list()----");
+        File dir2File = new File(workingDirectory, "\\FileTree\\Dir2");
+        String[] dir2Contents = dir2File.list();
+        for (int i = 0; i < dir2Contents.length; i++) {
+            System.out.println("i = " + i + ": " + dir2Contents[i]);
+        }
+
+        System.out.println("----print Dir2 contents using listFiles()----");
+        File[] dir2Files = dir2File.listFiles();
+        for (int i = 0; i < dir2Files.length; i++) {
+            System.out.println("i = " + i + ": " + dir2Files[i].getName());
         }
     }
 }
