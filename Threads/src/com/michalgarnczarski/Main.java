20219938ed1a3d008e9a67b6fcc9126d3127a910
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-08 08:14:54 +0200

    Video 266 finished

--- Threads/src/com/michalgarnczarski/Main.java
+++ Threads/src/com/michalgarnczarski/Main.java
@@ -21,6 +21,12 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous implementation of run().");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or timed out so I'm running again.");
+                } catch(InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted.");
+                }
             }
         });
         myRunnableThread.start();
