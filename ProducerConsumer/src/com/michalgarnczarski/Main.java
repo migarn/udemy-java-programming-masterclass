e2489512a935acdcde0a6260042c283ed7505831
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-18 16:01:05 +0200

    Video 273 done

--- ProducerConsumer/src/com/michalgarnczarski/Main.java
+++ ProducerConsumer/src/com/michalgarnczarski/Main.java
@@ -37,7 +37,9 @@ class MyProducer implements Runnable {
         for (String num : nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                buffer.add(num);
+                synchronized (buffer) {
+                    buffer.add(num);
+                }
 
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
@@ -46,7 +48,9 @@ class MyProducer implements Runnable {
         }
 
         System.out.println(color + "Adding EOF and exiting...");
-        buffer.add("EOF");
+        synchronized (buffer) {
+            buffer.add("EOF");
+        }
     }
 }
 
@@ -61,14 +65,16 @@ class MyConsumer implements Runnable {
 
     public void run() {
         while (true) {
-            if (buffer.isEmpty()) {
-                continue;
-            }
-            if (buffer.get(0).equals(EOF)) {
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
+            synchronized (buffer) {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
             }
         }
     }
