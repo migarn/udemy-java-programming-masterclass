32233d521e023fe7142018ca69a296f0b4a3a83d
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-15 09:44:21 +0200

    Video 293 done

--- Lambdas/src/com/michalgarnczarski/Main.java
+++ Lambdas/src/com/michalgarnczarski/Main.java
@@ -36,17 +36,39 @@ public class Main {
         employees.add(jack);
         employees.add(snow);
 
-        Collections.sort(employees, new Comparator<Employee>() {
-            @Override
-            public int compare(Employee employee1, Employee employee2) {
-                return employee1.getName().compareTo(employee2.getName());
-            }
-        });
+//        Collections.sort(employees, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee employee1, Employee employee2) {
+//                return employee1.getName().compareTo(employee2.getName());
+//            }
+//        });
+
+//        Collections.sort(employees, (Employee employee1, Employee employee2) ->
+//                employee1.getName().compareTo(employee2.getName()));
+
+        Collections.sort(employees, (employee1, employee2) ->
+                employee1.getName().compareTo(employee2.getName()));
 
         for (Employee employee : employees) {
             System.out.println(employee.getName());
         }
 
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        }, employees.get(0).getName(), employees.get(1).getName());
+
+        String sillyString = doStringStuff(((s1, s2) -> s1.toUpperCase() + s2.toUpperCase()),
+                employees.get(0).getName(), employees.get(1).getName());
+
+        System.out.println(sillyString);
+
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 
@@ -81,4 +103,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }
