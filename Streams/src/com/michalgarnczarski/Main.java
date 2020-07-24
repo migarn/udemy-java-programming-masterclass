9606f17fc723e33fc0da12400d5986e8174ed3b8
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-24 11:45:32 +0200

    Video 301 done

--- Streams/src/com/michalgarnczarski/Main.java
+++ Streams/src/com/michalgarnczarski/Main.java
@@ -3,6 +3,7 @@ package com.michalgarnczarski;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -33,5 +34,16 @@ public class Main {
                 .filter(s -> s.startsWith("G"))
                 .sorted()
                 .forEach(System.out::println);
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "071");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I17", "I29", "071");
+        Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
+        System.out.println("------------------------------------");
+        System.out.println(concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count());
+
+
     }
 }
