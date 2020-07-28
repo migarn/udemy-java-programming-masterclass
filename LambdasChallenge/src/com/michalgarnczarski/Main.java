0d39bcb344462d6d8b02fabd9c6c4236a1309983
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-28 18:50:04 +0200

    Video 304 done

--- LambdasChallenge/src/com/michalgarnczarski/Main.java
+++ LambdasChallenge/src/com/michalgarnczarski/Main.java
@@ -1,8 +1,11 @@
 package com.michalgarnczarski;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -45,6 +48,40 @@ public class Main {
         Supplier<String> iLoveJava = () -> "I Love Java";
 
         System.out.println(iLoveJava.get());
+
+        List<String> topNames2015 = Arrays.asList("Amelia", "Olivia", "emily", "Isla", "Ava", "oliver", "Jack", "Charlie", "harry", "Jacob");
+        List<String> firstUpperCaseList = new ArrayList<>();
+        topNames2015.forEach(name ->
+                firstUpperCaseList.add(name.substring(0,1).toUpperCase() + name.substring(1)));
+        firstUpperCaseList.sort((s1, s2) -> s1.compareTo(s2));
+        firstUpperCaseList.forEach(s -> System.out.println(s));
+
+        List<String> firstUpperCaseList2 = new ArrayList<>();
+        topNames2015.forEach(name ->
+                firstUpperCaseList2.add(name.substring(0,1).toUpperCase() + name.substring(1)));
+        firstUpperCaseList2.sort(String::compareTo);
+        firstUpperCaseList2.forEach(System.out::println);
+
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0,1).toUpperCase() + name.substring(1))
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+
+        long namesBeginningWithA = topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .filter(name -> name.startsWith("A"))
+                .count();
+
+        System.out.println("Number of names beginning with A is: " + namesBeginningWithA);
+
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .count();
     }
 
     public static String everySecondChar (String source) {
