14be832080257c7b7abfcc16471b3c7246fff8a9
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-08-17 15:56:16 +0200

    Video 317 done

---
+++ Banking/src/com/michalgarnczarski/BankAccountTest.java
@@ -0,0 +1,21 @@
+package com.michalgarnczarski;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("This test has yet to be implemented");
+    }
+}
