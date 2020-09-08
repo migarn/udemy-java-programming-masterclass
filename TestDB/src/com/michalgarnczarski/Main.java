70d226032e06975977a89eb22feb591685dbbac0
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-09-08 18:39:43 +0200

    Video 338 done

--- TestDB/src/com/michalgarnczarski/Main.java
+++ TestDB/src/com/michalgarnczarski/Main.java
@@ -1,17 +1,40 @@
 package com.michalgarnczarski;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	    try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:E:\\databases\\testjava.db");
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, pone INTEGER, email TEXT)");
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+
+//            statement.execute("INSERT into contacts (name, phone, email)" +
+//                    " VALUES('Tim', 654687, 'tim@email.com')");
+//            statement.execute("INSERT into contacts (name, phone, email)" +
+//                    " VALUES('Joe', 45678, 'joe@anywhere.com')");
+//            statement.execute("INSERT into contacts (name, phone, email)" +
+//                    " VALUES('Jane', 12341567, 'jane@somewhere.com')");
+//            statement.execute("INSERT into contacts (name, phone, email)" +
+//                    " VALUES('Fido', 15648, 'dog@email.com')");
+
+//            statement.execute("UPDATE contacts SET phone=111111111 WHERE name='Jane'");
+
+//            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+
+            while(results.next()) {
+                System.out.println(results.getString("name") + " " +
+                                    results.getInt("phone") + " " +
+                                    results.getString("email"));
+            }
+
+            results.close();
+
             statement.close();
             conn.close();
         } catch(SQLException e) {
