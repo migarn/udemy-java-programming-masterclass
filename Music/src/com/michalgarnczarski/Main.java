cdf40df78b5e9ce1783d2e52edea0072fc3e4c76
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-09-10 17:44:53 +0200

    Video 340 done

---
+++ Music/src/com/michalgarnczarski/Main.java
@@ -0,0 +1,16 @@
+package com.michalgarnczarski;
+
+import model.DataSource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DataSource dataSource = new DataSource();
+        if (!dataSource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        dataSource.close();
+    }
+}
