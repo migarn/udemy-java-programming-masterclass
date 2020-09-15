40fe309cb4ce260fe99dff8b36739c0fb3d2f04c
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-09-15 07:32:01 +0200

    Video 343 done

--- Music/src/com/michalgarnczarski/Main.java
+++ Music/src/com/michalgarnczarski/Main.java
@@ -24,6 +24,14 @@ public class Main {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
+        System.out.println("---------------------");
+
+        List<String> albumsForArtist = dataSource.queryAlbumsForArtist("Iron Maiden", DataSource.ORDER_BY_ASC);
+
+        for (String album : albumsForArtist) {
+            System.out.println(album);
+        }
+
         dataSource.close();
     }
 }
