1b2a28630139a109d839f09dc5bd6065a6ad4c38
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-03 19:53:24 +0200

    Video 357 done

--- MusicGUI/src/sample/Controller.java
+++ MusicGUI/src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 import sample.model.Album;
 import sample.model.Artist;
 import sample.model.DataSource;
@@ -14,9 +15,18 @@ public class Controller {
     @FXML
     private TableView artistTable;
 
+    @FXML
+    private ProgressBar progressBar;
+
+
+    @FXML
     public void listArtist() {
         Task<ObservableList<Artist>> task = new GetAllArtistsTask();
         artistTable.itemsProperty().bind(task.valueProperty());
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressBar.setVisible(true);
+        task.setOnSucceeded(e -> progressBar.setVisible(false));
+        task.setOnFailed(e -> progressBar.setVisible(false));
 
         new Thread(task).start();
     }
