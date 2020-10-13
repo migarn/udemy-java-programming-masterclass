c58573690873222fdaf2e666d2b3979f47c1e0a8
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-13 18:33:47 +0200

    Video 362 done

--- EchoClient/src/com/michalgarnczarski/Main.java
+++ EchoClient/src/com/michalgarnczarski/Main.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	    try (Socket socket = new Socket("localhost", 5000)) {
+	        socket.setSoTimeout(5000);
             BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
 
@@ -28,6 +30,8 @@ public class Main {
                 }
             } while (!echoString.equals("exit"));
 
+        } catch (SocketTimeoutException e) {
+            System.out.println("The socket timed out");
         } catch (IOException e) {
             System.out.println("Client error: " + e.getMessage());
         }
--- EchoServer/src/com/michalgarnczarski/Main.java
+++ EchoServer/src/com/michalgarnczarski/Main.java
@@ -13,9 +13,6 @@ public class Main {
 	    try(ServerSocket serverSocket = new ServerSocket(5000)) {
             while (true) {
                 new Echoer(serverSocket.accept()).start();
-//                Socket socket = serverSocket.accept();
-//                Echoer echoer = new Echoer(socket);
-//                echoer.start();
             }
 
         } catch (IOException e) {
