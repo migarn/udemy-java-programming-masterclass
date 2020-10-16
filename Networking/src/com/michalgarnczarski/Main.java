d20aa265c7867bbd30fd0c8fba42e039b327950b
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-16 17:46:09 +0200

    Video 365 done

--- Networking/src/com/michalgarnczarski/Main.java
+++ Networking/src/com/michalgarnczarski/Main.java
@@ -1,37 +1,34 @@
 package com.michalgarnczarski;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	 try {
-//	     URI uri = new URI("db://username:pasword@myserver.com:5000/catalogue/phones?os=android#samsung");
-         URI baseUri = new URI("http://username:pasword@myserver.com:5000");
-         URI uri = new URI("/catalogue/phones?os=android#samsung");
-         URI resolvedUri = baseUri.resolve(uri);
+	     URL url = new URL("http://example.org");
+         URLConnection urlConnection = url.openConnection();
+         urlConnection.setDoOutput(true);
+         urlConnection.connect();
 
-	     URL url = resolvedUri.toURL();
-         System.out.println("URL = " + url);
+         BufferedReader inputStream = new BufferedReader(
+                 new InputStreamReader(urlConnection.getInputStream()));
 
-//         System.out.println("Scheme = " + uri.getScheme());
-//         System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-//         System.out.println("Authority = " + uri.getAuthority());
-//         System.out.println("User info = " + uri.getUserInfo());
-//         System.out.println("Host = " + uri.getHost());
-//         System.out.println("Port = " + uri.getPort());
-//         System.out.println("Path = " + uri.getPath());
-//         System.out.println("Query = " + uri.getQuery());
-//         System.out.println("Fragment = " + uri.getFragment());
+         String line = "";
 
-     } catch (URISyntaxException e) {
-         System.out.println("URI Bad Syntax: " + e.getMessage());
+         while (line != null) {
+             line = inputStream.readLine();
+             System.out.println(line);
+         }
+         inputStream.close();
+
      } catch (MalformedURLException e) {
-         System.out.println("URL Malformed: " + e.getMessage());
+         System.out.println("Malformed URL: " + e.getMessage());
+     } catch (IOException e) {
+         System.out.println("IOException: " + e.getMessage());
      }
-
     }
 }
